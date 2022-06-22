f4df0333f01f8f39539e245dbac10b1c76af95a2
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2022-06-22 11:58:50 +0100

    add axios useEffect to API call

--- src/App.js
+++ src/App.js
@@ -1,22 +1,90 @@
+// console.log("npm install axios")
 
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const [username, setUsername] = useState('');
+  
+  const [repos, setRepos] = useState([]);
+  
+  const [search, setSearch] = useState('');
+  const [error, setError] = useState('');
+  const [statusMessage, setStatusMessage] = useState('Loading');
+
+
+
+  useEffect(() => {
+
+    const fetchRepos = async (searchTerm) => {
+      searchTerm ||= 'laylasouthcombe'
+      try {
+        const url = `https://api.github.com/users/${searchTerm}/repos`
+
+        // const { data: { students } } = await axios.get(url)
+        const { data } = await axios.get(url)
+        console.log(data)
+        setRepos(data)
+        setStatusMessage('')
+        setError('')
+      } catch (err) {
+        setError(err)
+        setStatusMessage('Loading...')
+      }
+    }
+    const timeoutId = setTimeout(() => {
+      fetchRepos(search)
+    }, 400);
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+
+  }, [search])
+
+  // nothing => useEffect will run like crazy
+  // [] => useEffect will run once
+  // [search] => useEffect will run everytime the value changes
+
+  console.log(username)
+  const renderedRepos = repos.map(r => {
+    return (
+      <li key={r.github}>{r.name}</li>
+    )
+  })
+
+  const onInputChange = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const onFormSubmit = (e) => {
+    e.preventDefault()
+    setSearch(username)
+    setUsername('')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        {error
+          ? <h1>Sorry, we could not find a(n) {search} username</h1>
+          : <div>
+            <h3> {statusMessage ? statusMessage : ''} </h3>
+            <ul> {renderedRepos} </ul>
+          </div>
+        }
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={onFormSubmit}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={onInputChange}
+          />
+        </form>
       </header>
     </div>
   );
